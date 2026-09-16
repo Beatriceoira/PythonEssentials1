41606e88b9d972db472bbc5f78c72e686cf00a92
Beatrice Claire Oira <89373884+Beatriceoira@users.noreply.github.com>
2026-09-16 08:55:09 +0000

    Added the code for lab 2.1.13
    Minimized print calls and created larger arrow patterns.

---
+++ print()function_and_its_arguments/2_1_13.py
@@ -0,0 +1,22 @@
+# Minimize print() calls using \n
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+
+# Make the arrow twice as large
+print("        **")
+print("       ** **")
+print("      **   **")
+print("     **     **")
+print("******     ******")
+print("      **   **")
+print("      **   **")
+print("      *******")
+
+# Two arrows side by side
+print("    *       *")
+print("   * *     * *")
+print("  *   *   *   *")
+print(" *     * *     *")
+print("***   *****   ***")
+print("  *   *   *   *")
+print("  *   *   *   *")
+print("  *****   *****")
